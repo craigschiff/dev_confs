a52346e095026e193fd80563e56b2bb7750f4beb
fattyduck <dison.ruan@yahoo.com>
2017-04-21 15:06:21 -0400

    add add image

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,7 +1,11 @@
 class EventSerializer < ActiveModel::Serializer
   attributes :id, :name, :description,
   :date, :website, :address, :perks, :cost,
-  :city, :organizer, :topic
+  :city, :organizer, :image
+
+  belongs_to :city, serializer: CitySerializer
+  belongs_to :organizer, serializer: OrganizerSerializer
+  belongs_to :topic, serializer: TopicSerializer
   has_many :rsvps
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :profiles
     resources :events
     resources :sessions, only: [:create]
-    resources :registrations, only: [:create, :show]
+    resources :registrations, only: [:create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
