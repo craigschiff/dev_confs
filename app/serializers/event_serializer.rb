8b8431f7657745f560facc11cbe0280fc2e3f6e9
github username <github email address>
2017-04-23 15:17:15 -0400

    routes and setting up testing

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,7 +1,7 @@
 class EventSerializer < ActiveModel::Serializer
   attributes :id, :name, :description,
   :date, :website, :address, :perks, :cost,
-  :city, :organizer, :topic
+  :city, :organizer, :topic, :image
   has_many :rsvps
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     resources :events
     resources :sessions, only: [:create]
     resources :registrations, only: [:create, :show]
+    post '/events/:id', to: 'events#update'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
