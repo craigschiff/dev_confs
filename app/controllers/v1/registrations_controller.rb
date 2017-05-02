a134d3817eef04728dbe636fdd745441a749d081
fattyduck <dison.ruan@yahoo.com>
2017-05-02 10:36:21 -0400

    nothing much

--- app/controllers/v1/registrations_controller.rb
+++ app/controllers/v1/registrations_controller.rb
@@ -15,4 +15,17 @@ class V1::RegistrationsController < ApplicationController
   def account_params
     params.require(:account).permit(:username, :password)
   end
+
+  class Auth
+    SECRET = Rails.application.secrets.jwt_secret
+
+    def self.issue(payload)
+      JWT.encode(payload, SECRET)
+    end
+
+    def self.decode(token)
+      JWT.decode(token, SECRET).first
+    end
+  end
+
 end
